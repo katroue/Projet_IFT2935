3e28d2d546a879da8b6252f96a65575b686aae1c
Katherine Demers <katherine.demers@umontreal.ca>
2024-04-08 18:40:38 -0400

    Update base_de_donnée_projet.sql

--- base_de_donnée_projet.sql
+++ base_de_donnée_projet.sql
@@ -23,6 +23,8 @@ CREATE TABLE Utilisateur ( -- comprend admin et membre
 
 CREATE TABLE Administrateur (
     id_admin int NOT NULL primary key,
+    courriel VARCHAR(255),
+    pseudo VARCHAR(255),
     nom VARCHAR(255),
     prénom VARCHAR(255)
 );
@@ -136,12 +138,12 @@ INSERT INTO Utilisateur (type) VALUES
 ('Admin'),
 ('Membre');
 
-INSERT INTO Administrateur (id_admin, nom, prénom) VALUES 
-(1, 'Durand', 'Alice'),
-(3, 'Lefebvre', 'Bob'),
-(5, 'Moreau', 'Charlie'),
-(7, 'Simon', 'Danielle'),
-(9, 'Laurent', 'Eve');
+INSERT INTO Administrateur (id_admin, courriel, pseudo, nom, prénom) VALUES 
+(1, 'alice.durand@gmail.com', 'aliced', 'Durand', 'Alice'),
+(3, 'bob.lefebvre@gmail.com', 'bobl', 'Lefebvre', 'Bob'),
+(5, 'charlie.moreau@gmail.com', 'charliem', 'Moreau', 'Charlie'),
+(7, 'danielle.simon@gmail.com', 'danielles', 'Simon', 'Danielle'),
+(9, 'eve.laurent@gmail.com', 'evel','Laurent', 'Eve');
 
 INSERT INTO Membre (id_membre, pseudo, nom, prénom, courriel, date_naissance, id_admin_ajout) VALUES 
 (2, 'jdoe', 'Doe', 'John', 'jdoe@example.com', '1990-01-01', 1),
@@ -348,143 +350,3 @@ INSERT INTO Evaluation (nom_fichier, rating, id_utilisateur, rating_date) VALUES
 ('cours_mathematiques.mp4', 4, 7, '2024-03-25'),
 ('salle_classe.jpg', 3, 6, '2024-03-25'),
 ('salle_classe.jpg', 2, 5, '2024-03-28');
-
-
--- Requêtes -- 
-
--- Donner le nom et le prénom et le nombre de fichiers ajoutés de tous les admins qui ont ajouté au moins 1 fichier vidéo
-SELECT a.nom, a.prénom, COUNT(*) AS NombreDeVideos
-FROM Administrateur a
-JOIN AjoutFichier j ON a.id_admin = j.id_utilisateur
-JOIN Fichier f ON j.nom_fichier = f.nom_fichier
-WHERE f.type = 'Video'
-GROUP BY a.id_admin, a.nom, a.prénom
-HAVING COUNT(*) > 1;
-
--- Donner le nom des fichiers et le(s) nom et prénoms de l'utilisateur qui a ajouté le/les fichiers qui sont associé au mot clé 'Musique'
-SELECT u.id_utilisateur, aj.nom_fichier
-FROM MotClé m
-JOIN AssociationMotClé am ON m.mot = am.mot_clé
-JOIN AjoutFichier aj ON am.nom_fichier = aj.nom_fichier
-JOIN Utilisateur u ON aj.id_utilisateur = u.id_utilisateur
-WHERE m.mot = 'Musique'
-GROUP BY u.id_utilisateur, aj.nom_fichier;
-
--- Donner le nom des fichiers, leurs date d'ajout, leurs date de visionnement, et les utilisateurs qui les ont ajoutés, des fichiers qui ont été visionnés entre le 2024-03-09 et le 2024-03-15
-SELECT v.view_date, f.nom_fichier, f.date_ajout, u.id_utilisateur
-FROM Visionnement v
-JOIN Fichier f ON f.nom_fichier = v.nom_fichier
-JOIN AjoutFichier aj ON aj.nom_fichier = f.nom_fichier
-JOIN Utilisateur u ON u.id_utilisateur = aj.id_utilisateur
-WHERE v.view_date BETWEEN '2024-03-09' AND '2024-03-15'
-GROUP BY f.nom_fichier, u.id_utilisateur, v.view_date, f.date_ajout;
-
--- Donner, pour tous les administrateurs, les noms et prénoms des membres et les noms des fichiers et leurs types qu'ils ont ajoutés
-SELECT a.id_admin, m.nom AS nom_membre, m.prénom AS prénom_membre, f.nom_fichier, f.type AS type_fichier
-FROM Administrateur a 
-JOIN Membre m ON a.id_admin = m.id_admin_ajout
-JOIN AjoutFichier af ON a.id_admin = af.id_utilisateur
-JOIN Fichier f ON af.nom_fichier = f.nom_fichier
-GROUP BY a.id_admin, m.nom, m.prénom, f.nom_fichier, f.type;
-
--- Donner, pour tous les fichiers, le nombre de visionnement et la moyenne de leurs évaluations et l'id des utilisateurs qui l'a ajouté
-SELECT f.nom_fichier, COUNT(DISTINCT v.id) AS nombre_visionnement, AVG(e.rating) AS evaluation_moyenne, aj.id_utilisateur
-FROM Fichier f
-LEFT JOIN Visionnement v ON f.nom_fichier = v.nom_fichier
-LEFT JOIN Evaluation e ON f.nom_fichier = e.nom_fichier
-LEFT JOIN AjoutFichier aj ON f.nom_fichier = aj.nom_fichier
-GROUP BY f.nom_fichier, aj.id_utilisateur;
-
--- Donner tous les noms et prénoms des membres qui n'ont pas ajouté de fichiers mais qui ont donné au moins une évaluation à un fichier
-SELECT m.nom, m.prénom, COUNT(e.id) AS Nombre_évaluation
-FROM Membre m 
-LEFT JOIN AjoutFichier aj ON m.id_membre = aj.id_utilisateur
-LEFT JOIN Evaluation e ON e.nom_fichier = aj.nom_fichier AND e.id_utilisateur = m.id_membre
-WHERE aj.nom_fichier IS NULL
-GROUP BY m.nom, m.prénom, aj.nom_fichier;
-
--- Donner en ordre décroissant les fichiers selon leurs moyennes d'évaluation avec une précision de deux chiffres après la virgule
-SELECT e.nom_fichier, ROUND(AVG(CAST(e.rating AS DECIMAL(3,1))), 1) AS Moyenne_Evaluation
-FROM Evaluation e 
-GROUP BY e.nom_fichier
-ORDER BY Moyenne_Evaluation DESC;
-
--- Donner, pour tous les utilisateurs, leurs types, leurs noms et prénoms
-SELECT u.id_utilisateur,u.type,
-  CASE WHEN u.type = 'Admin' THEN a.nom ELSE m.nom END AS nom,
-  CASE WHEN u.type = 'Admin' THEN a.prénom ELSE m.prénom END AS prénom
-FROM Utilisateur u
-LEFT JOIN Administrateur a ON u.id_utilisateur = a.id_admin AND u.type = 'Admin'
-LEFT JOIN Membre m ON u.id_utilisateur = m.id_membre AND u.type = 'Membre';
-
--- Donner pour tous les synonymes d'un mot-clé, le mot-clé et le fichier correspondant
-SELECT s.synonyme, s.mot_clé, a.nom_fichier
-FROM SynonymeMotClé s 
-JOIN AssociationMotClé a ON s.mot_clé = a.mot_clé
-GROUP BY s.synonyme, s.mot_clé, a.nom_fichier;
-
--- Donner pour tous les membres, le nom de famille des administrateurs qui les ont ajoutés
-SELECT m.id_membre, m.nom, m.prénom, a.nom as Admin_Ajout 
-FROM Membre m 
-JOIN Administrateur a ON m.id_admin_ajout = a.id_admin
-GROUP BY m.id_membre, m.nom, m.prénom, a.nom;
-GO
-
-CREATE OR ALTER PROCEDURE ViewCount
-AS
-BEGIN
-    SELECT f.nom_fichier, COUNT(DISTINCT v.id) AS nombre_visionnement
-    FROM Fichier f
-    LEFT JOIN Visionnement v ON f.nom_fichier = v.nom_fichier
-    GROUP BY f.nom_fichier
-END;
-GO
-
-CREATE OR ALTER FUNCTION GetAverageRating(@VideoName VARCHAR(255))
-RETURNS DECIMAL(3, 2)
-AS
-BEGIN
-    DECLARE @AverageRating DECIMAL(3, 2);
-
-    SELECT @AverageRating = AVG(CAST(rating AS DECIMAL(10,2)))
-    FROM Evaluation
-    WHERE nom_fichier = @VideoName;
-
-    RETURN @AverageRating;
-END;
-GO
-
-SELECT dbo.GetAverageRating('sketch_comique.mp4') AS AverageRating;
-GO
-
-CREATE OR ALTER PROCEDURE GetAverageVideoRatings
-AS
-BEGIN
-    SELECT 
-        nom_fichier AS VideoName,
-        AVG(rating) AS AverageRating
-    FROM Evaluation
-    GROUP BY nom_fichier
-    ORDER BY AverageRating DESC;
-END;
-GO
-
-EXEC GetAverageVideoRatings;
-GO
-
-CREATE OR ALTER PROCEDURE GetUserActivity
-AS
-BEGIN
-    SELECT 
-        u.pseudo AS Username,
-        COUNT(e.id) AS RatingsCount,
-        COUNT(a.nom_fichier) AS UploadsCount
-    FROM Membre u
-    LEFT JOIN Evaluation e ON u.id_membre = e.id_utilisateur
-    LEFT JOIN AjoutFichier a ON u.id_membre = a.id_utilisateur
-    GROUP BY u.pseudo;
-END;
-GO
-
-EXEC GetUserActivity;
-GO
